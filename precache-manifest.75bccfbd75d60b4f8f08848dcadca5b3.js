2fe09df6775c524d4e04084651fffc328b7e2d24
Awais S. Khan <awais.khan725@gmail.com>
2020-06-06 15:44:45 +1000

    Updates

---
+++ precache-manifest.75bccfbd75d60b4f8f08848dcadca5b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29cb8ed09ae7a506c033d47d6bfc2ff5",
+    "url": "/fnf-library/index.html"
+  },
+  {
+    "revision": "a75fd0640002be5dff05",
+    "url": "/fnf-library/static/css/main.8889b50d.chunk.css"
+  },
+  {
+    "revision": "5f97e107218762c8d43f",
+    "url": "/fnf-library/static/js/2.b4fa165c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fnf-library/static/js/2.b4fa165c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75fd0640002be5dff05",
+    "url": "/fnf-library/static/js/main.fc2c3cd2.chunk.js"
+  },
+  {
+    "revision": "786a6d757f61b23870b8",
+    "url": "/fnf-library/static/js/runtime-main.e2bd8728.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/fnf-library/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fnf-library/precache-manifest.75bccfbd75d60b4f8f08848dcadca5b3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fnf-library/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
